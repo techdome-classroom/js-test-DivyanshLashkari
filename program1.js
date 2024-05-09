c4059a5666298432ee3493e6fad1706ccdc88794
Divyansh01~ <divyansh63lashkari@gmail.com>
2024-05-09 13:13:30 +0530

    first commit

--- program1.js
+++ program1.js
@@ -1,10 +1,36 @@
 const getTotalIsles = function (grid) {
+  // write your code here
+  const rows = grid.length;
+  const cols = grid[0].length;
+  let count = 0;
 
+  function dfs(i, j) {
+    if (i < 0 || i >= rows || j < 0 || j >= cols || grid[i][j] === "L") {
+      return;
+    }
 
-    // write your code here
+    grid[i][j] = "V";
 
-  };
-  
-  module.exports = getTotalIsles;
+    dfs(i + 1, j);
+    dfs(i - 1, j);
+    dfs(i, j + 1);
+    dfs(i, j - 1);
+  }
 
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      if (grid[i][j] === 'L' && (i === 0 || grid[i - 1][j] !== 'L') && (j === 0 || grid[i][j - 1] !== 'L')) {
 
+        count++;
+
+        dfs(i, j);
+
+      }
+    }
+  }
+  console.log(count);
+
+  return count;
+};
+
+module.exports = getTotalIsles;
--- program2.js
+++ program2.js
@@ -1,6 +1,16 @@
 const decodeTheRing = function (s, p) {
 
     // write your code here
+    if(s.lenght !== p.lenght){
+      return false;
+    }
+
+    for(let i=0; i<s.lenght; i++){
+      if(s[i] !== p[i] && p[i] !== '?'){
+        return false;
+      }
+    }
+    return true
 
   };
   
